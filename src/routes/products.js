1c91f6797f06a221265c9af6f5f4b53481563cac
adrianc <adrianc@dbserver.com.br>
2017-08-11 00:44:15 -0300

    fixed product route with ProductsController instance!

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import productController from '../controllers/products';
+import ProductsController from '../controllers/products';
 
 const router = express.Router();
+const productsController = new ProductsController();
 
 router.get('/', (req, res) => productController.get(req, res));
 
