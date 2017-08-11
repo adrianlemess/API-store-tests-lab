f373f1d2dbf12b041fa39cfdf2c4e2c48a246bd7
adrianc <adrianc@dbserver.com.br>
2017-08-11 00:30:44 -0300

    add  products controller and refactor routes. change slow variable in mocha.opts

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,13 +1,8 @@
 import express from 'express';
+import productController from '../controllers/products';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send([{
-        name: 'Default Product',
-        description: 'product description',
-        price: 100
-    }]);
-});
+router.get('/', productController.get);
 
 export default router;
