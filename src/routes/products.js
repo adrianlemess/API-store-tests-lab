160d2960fb65f42e5cbbec8ac32e14c6f1a87ce7
adrianc <adrianc@dbserver.com.br>
2017-08-11 00:39:11 -0300

    fixed product route

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,6 +3,6 @@ import productController from '../controllers/products';
 
 const router = express.Router();
 
-router.get('/', productController.get);
+router.get('/', (req, res) => productController.get(req, res));
 
 export default router;
